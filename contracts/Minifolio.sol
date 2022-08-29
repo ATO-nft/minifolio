616833bece64d22fe9c888dd328eec74f58090a5
julienbrg <julien@strat.cc>
2022-08-29 22:58:40 +0200

    add receiver

--- contracts/Minifolio.sol
+++ contracts/Minifolio.sol
@@ -3,6 +3,7 @@ pragma solidity 0.8.15;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
@@ -121,7 +122,7 @@ contract Minifolio is ERC721, RedeemableURIStorage, ERC721Burnable, Ownable, ERC
 		require(ownerOf(tokenId) == msg.sender, "You are not the owner of this token");
 		ERC20(btcAddress).transfer(ownerOf(tokenId), ERC20(btcAddress).balanceOf(address(this)));
 		payable(ownerOf(tokenId)).transfer(address(this).balance);
-        // add an nft transfer here
+		// add an nft transfer here
 		super.redeem(tokenId);
 	}
 
